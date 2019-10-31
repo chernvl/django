6b385d78436c234383919c7ddb4b88167d0f915d
chernvl <52606344+chernvl@users.noreply.github.com>
2019-10-31 17:00:53 +0300

    Update urls.py
    news url added

--- mysite/mainApp/urls.py
+++ mysite/mainApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^news/$', views.contacts, name = 'news'),
     url(r'^contacts/$', views.contacts, name = 'contacts')
 ]
