f9691602c0c839f58876e5e5f42ba97e63f2fb08
bondarchukeugene <57070772+bondarchukeugene@users.noreply.github.com>
2019-11-04 19:33:40 +0300

    Update urls.py

--- mysite/mainApp/urls.py
+++ mysite/mainApp/urls.py
@@ -1,9 +1,14 @@
+rom django.contrib import admin
+from django.urls import path
 from django.conf.urls import url
-from django.conf.urls import include
-from . import views
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^contacts/$', views.contacts, name = 'contacts'),
-url(r'^upload/$', views.upload, name = 'upload')
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('mainApp.urls')),
+    url(r'^webexample/', include('webexample.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
